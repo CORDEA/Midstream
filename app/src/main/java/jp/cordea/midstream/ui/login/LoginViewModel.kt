cb4b05dc941c7433f4a581ac36be5563f86aa537
Yoshihiro Tanaka <contact@cordea.jp>
2018-06-24 14:21:30 +0900

    initial commit :)

---
+++ app/src/main/java/jp/cordea/midstream/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package jp.cordea.midstream.ui.login
+
+import android.arch.lifecycle.ViewModel;
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/jp/cordea/midstream/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package jp.cordea.midstream.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
