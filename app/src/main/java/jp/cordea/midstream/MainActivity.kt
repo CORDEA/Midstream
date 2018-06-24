b0b44897c4688478211f16e7605cda2c3ca95522
Yoshihiro Tanaka <contact@cordea.jp>
2018-06-24 16:29:40 +0900

    inject dependencies

--- app/src/main/java/jp/cordea/midstream/MainActivity.kt
+++ app/src/main/java/jp/cordea/midstream/MainActivity.kt
@@ -2,15 +2,28 @@ package jp.cordea.midstream
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
     }
 
     override fun onSupportNavigateUp(): Boolean =
             findNavController(this, R.id.nav_host_fragment).navigateUp()
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> =
+            dispatchingAndroidInjector
 }
