1c3509d9b601f63d73b93bbb504e3f80f50dfa8d
Yoshihiro Tanaka <contact@cordea.jp>
2018-07-07 18:49:10 +0900

    implement fragment

--- app/src/main/java/jp/cordea/midstream/ui/main/MainFragment.kt
+++ app/src/main/java/jp/cordea/midstream/ui/main/MainFragment.kt
@@ -1,28 +1,72 @@
 package jp.cordea.midstream.ui.main
 
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import jp.cordea.midstream.R
+import androidx.lifecycle.Observer
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.ViewHolder
+import dagger.android.support.AndroidSupportInjection
+import jp.cordea.midstream.databinding.MainFragmentBinding
 import javax.inject.Inject
+import javax.inject.Provider
 
 class MainFragment : Fragment() {
 
     companion object {
+        private const val REQUEST_AUTHORIZATION = 3
+
         fun newInstance() = MainFragment()
     }
 
     @Inject
     lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    @Inject
+    lateinit var listItem: Provider<MainListItem>
+
+    private val adapter = GroupAdapter<ViewHolder>()
+
+    override fun onAttach(context: Context?) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
     }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View = MainFragmentBinding.inflate(inflater, container, false).root
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        viewModel.notifyStateChanged
+                .observe(this, Observer {
+                    when (it) {
+                        is MainListState.BusDataChanged -> {
+                        }
+                        is MainListState.AuthRequired -> {
+                            startActivityForResult(
+                                    it.intent,
+                                    REQUEST_AUTHORIZATION
+                            )
+                        }
+                        is MainListState.ErrorOccurred -> {
+                        }
+                    }
+                })
+        viewModel.fetch()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_AUTHORIZATION && resultCode == RESULT_OK) {
+            viewModel.fetch()
+        }
     }
 }
