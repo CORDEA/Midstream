e77a56f87349867a1f0b1cbdbe3b75e33e89b03a
Yoshihiro Tanaka <contact@cordea.jp>
2018-07-08 14:05:35 +0900

    modify layout of list item

--- app/src/main/java/jp/cordea/midstream/ui/main/MainListItem.kt
+++ app/src/main/java/jp/cordea/midstream/ui/main/MainListItem.kt
@@ -1,12 +1,17 @@
 package jp.cordea.midstream.ui.main
 
 import com.xwray.groupie.databinding.BindableItem
+import jp.cordea.midstream.BusData
 import jp.cordea.midstream.R
 import jp.cordea.midstream.databinding.MainListItemBinding
 import javax.inject.Inject
 
 class MainListItemModel(
-)
+        private val data: BusData
+) {
+    val title: String = data.arrivalTime.toString()
+    val description = ""
+}
 
 class MainListItem @Inject constructor(
 ) : BindableItem<MainListItemBinding>() {
@@ -15,5 +20,6 @@ class MainListItem @Inject constructor(
     override fun getLayout(): Int = R.layout.main_list_item
 
     override fun bind(binding: MainListItemBinding, position: Int) {
+        binding.model = model
     }
 }
